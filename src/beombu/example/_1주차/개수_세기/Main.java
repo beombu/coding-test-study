975de77f19e373832d639c1e0eaff017f1627d6a
신범철 <sunil1369@easyshop.co.kr>
2025-12-26 09:43:01 +0900

    Feat : 개수 세기

---
+++ src/beombu/example/_1주차/개수_세기/Main.java
@@ -0,0 +1,29 @@
+package beombu.example._1주차.개수_세기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        int v = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if(v == Integer.parseInt(st.nextToken())) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
