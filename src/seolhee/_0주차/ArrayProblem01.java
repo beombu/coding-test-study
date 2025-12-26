07eae41490bd7e1fd24a6ddac8eac30cf031941f
한 설희 <seolhee.han@easyshop.co.kr>
2025-12-26 12:00:21 +0900

    Style: 백준 10818 - 최소, 최대

--- src/seolhee/_0주차/ArrayProblem01.java
+++ src/seolhee/_0주차/ArrayProblem01.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class MaxAndMin1 {
+public class ArrayProblem01 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try{
